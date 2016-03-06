559379cf190498594ea65ed5fffe9a2454a0db31
Miguel Espinoza <wiseit@outlook.com>
2016-03-07 00:28:17 +0100

    Refactor: This routine should separate commands and params

--- options.cpp
+++ options.cpp
@@ -1,13 +1,29 @@
 #include "options.h"
 #include <string>
 #include <vector>
-
+#include <map>
 /**
  * @brief Stores a vector of parameters comming from command line
  * @param args vector of strings
  */
-void Options::registerParams(std::vector<std::string> args){
-    this->cli_params = args;
+void Options::registerArgs(std::vector<std::string> args){
+    std::map<std::string, std::string> parts;
+    std::map<std::string, std::string> commands;
+    for(std::vector<Param>::iterator param_ite = this->internal_params.begin(); param_ite != this->internal_params.end(); param_ite++) {
+        for (std::vector<std::string>::iterator args_ite = args.begin(); args_ite != args.end(); args_ite++) {
+            parts = this->getParts( (*args_ite));
+            if (this->is_command(parts.begin()->first)) {
+                commands[parts.begin()->first] = parts.begin()->second;
+            } else {
+                // The Param ignores invalid assignments
+                (*param_ite).setValue(parts.begin()->first, parts.begin()->second);
+            }
+        }
+    }
+
+    // This might fail when there are more than one command
+    this->run_command(commands);
+}
 }
 
 void Options::addParam(Param orig){}
