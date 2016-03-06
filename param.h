c4b95ac16cc2aa43432934d197ce03a5742f4dac
Miguel Espinoza <wiseit@outlook.com>
2016-03-07 00:33:40 +0100

    Feature: Getters and setters

--- param.h
+++ param.h
@@ -9,12 +9,16 @@ class Param {
     std::string     str_value;
 
 public:
+    std::string getShortForm();
+    std::string getLongForm();
+    std::string getValue();
     std::string getDescription();
     void setDescription(std::string);
     void setShortForm(char);
     void setLongForm(std::string);
-    void setType(std::string);
     void setValue(std::string);
+    void setValue(int);
+    void setValue(bool);
     void setValue(std::string, std::string);
     void requires(Param);
 };
